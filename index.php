d8bf0fe8a12914ddb21cc1ac2f09c330a1d157eb
Md Tuhin Al Jobayer <58110053+Eagl3Eyes@users.noreply.github.com>
2024-08-12 09:50:25 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,75 @@
+<?php
+session_start();
+
+
+
+$users = [
+    [
+        "first_name" => "John",
+        "last_name" => "Doe",
+        "username" => "user1",
+        "email" => "user1@gmail.com",
+        "password" => "password1",
+        "profile_img" => "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "cover_img" => "https://www.trendycovers.com/covers/Colorful_Hearts_facebook_cover_1346950504.jpg",
+        'location' => 'New York, USA',
+        'work' => 'Deft Group',
+        'education' => 'University X',
+        'joined' => 'March 2022'
+    ],
+    [
+        "first_name" => "Jane",
+        "last_name" => "Smith",
+        "username" => "user2",
+        "email" => "user2@gmail.com",
+        "password" => "password2",
+        "profile_img" => "https://wallpapers.com/images/featured-full/cool-profile-picture-87h46gcobjl5e4xu.jpg",
+        "cover_img" => "https://www.trendycovers.com/covers/abstract_3d_facebook_cover_1370594397.jpg",
+        'location' => 'New Jersy, USA',
+        'work' => 'Deft Decor',
+        'education' => 'University Y',
+        'joined' => 'March 2014'
+    ],
+    [
+        "first_name" => "Alice",
+        "last_name" => "Johnson",
+        "username" => "user3",
+        "email" => "user3@gmail.com",
+        "password" => "password3",
+        "profile_img" => "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "cover_img" => "https://149369349.v2.pressablecdn.com/wp-content/uploads/2012/10/twitter-cover.jpg",
+        'location' => 'New York, USA',
+        'work' => 'Deft Digital',
+        'education' => 'University Z',
+        'joined' => 'March 2020'
+    ]
+
+];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $email = $_POST['email'];
+    $username = $_POST['email'];
+    $password = $_POST['password'];
+
+
+    foreach ($users as $user) {
+        if (($user['email'] === $email || $user['username'] === $username) && $user['password'] === $password) {
+
+            $_SESSION['user'] = $user;
+
+            $_SESSION['last_activity'] = time();
+
+            header("Location: profile.php");
+            exit();
+        }
+    }
+
+    echo "<div class='text-red-500 text-center mt-4'>Invalid email or password.</div>";
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -56,79 +128,7 @@
 
 
 
-    <?php
-    ob_start();
-    session_start();
-    
 
-
-    $users = [
-        [
-            "first_name" => "John",
-            "last_name" => "Doe",
-            "username" => "user1",
-            "email" => "user1@gmail.com",
-            "password" => "password1",
-            "profile_img" => "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            "cover_img" => "https://www.trendycovers.com/covers/Colorful_Hearts_facebook_cover_1346950504.jpg",
-            'location' => 'New York, USA',
-            'work' => 'Deft Group',
-            'education' => 'University X',
-            'joined' => 'March 2022'
-        ],
-        [
-            "first_name" => "Jane",
-            "last_name" => "Smith",
-            "username" => "user2",
-            "email" => "user2@gmail.com",
-            "password" => "password2",
-            "profile_img" => "https://wallpapers.com/images/featured-full/cool-profile-picture-87h46gcobjl5e4xu.jpg",
-            "cover_img" => "https://www.trendycovers.com/covers/abstract_3d_facebook_cover_1370594397.jpg",
-            'location' => 'New Jersy, USA',
-            'work' => 'Deft Decor',
-            'education' => 'University Y',
-            'joined' => 'March 2014'
-        ],
-        [
-            "first_name" => "Alice",
-            "last_name" => "Johnson",
-            "username" => "user3",
-            "email" => "user3@gmail.com",
-            "password" => "password3",
-            "profile_img" => "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            "cover_img" => "https://149369349.v2.pressablecdn.com/wp-content/uploads/2012/10/twitter-cover.jpg",
-            'location' => 'New York, USA',
-            'work' => 'Deft Digital',
-            'education' => 'University Z',
-            'joined' => 'March 2020'
-        ]
-
-    ];
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $email = $_POST['email'];
-        $username = $_POST['email'];
-        $password = $_POST['password'];
-
-
-        foreach ($users as $user) {
-            if (($user['email'] === $email || $user['username'] === $username) && $user['password'] === $password) {
-
-                $_SESSION['user'] = $user;
-
-                $_SESSION['last_activity'] = time();
-
-                header("Location: profile.php");
-                exit();
-            }
-        }
-
-        echo "<div class='text-red-500 text-center mt-4'>Invalid email or password.</div>";
-    }
-
-    ob_end_flush();
-    ?>
 </body>
 
 </html>
